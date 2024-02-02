aae1a786c755f75a3f573e9fbe6bc4be5c7a0978
valievma <maksim.v1liev@gmail.com>
2024-02-02 17:56:46 +0300

    new version

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import requests
+from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel
 
@@ -11,13 +12,21 @@ SCREEN_SIZE = [600, 450]
 class Application(QWidget):
     def __init__(self):
         super().__init__()
-        self.get_image(0.05)
-        self.init_ui()
+        self.mash = 0.005
+        self.l1 = None
+        self.l2 = None
+        self.map_file = "map.png"
 
-    def get_image(self, mash: float = 0.02, l1: float = 37.530887, l2: float = 55.703118):
+        self.init_ui()
+        self.set_image(self.mash)
+
+    def set_image(self, mash: float = 0.02, l1: float = 37.530887, l2: float = 55.703118):
         m = f'{mash},{mash}'
 
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll={l1},{l2}&spn={m}&l=map"
+        self.l1 = l1
+        self.l2 = l2
+
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.l1},{self.l2}&spn={m}&l=map"
         response = requests.get(map_request)
 
         if not response:
@@ -26,22 +35,38 @@ class Application(QWidget):
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
 
-        self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
 
-    def init_ui(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-
+    def update_image(self):
         self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
         self.image.move(0, 0)
         self.image.resize(600, 450)
         self.image.setPixmap(self.pixmap)
+        self.image.show()
+
+    def init_ui(self):
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+
+        self.update_image()
 
     def close_event(self, event):
         os.remove(self.map_file)
+
+    def keyPressEvent(self, event):
+        clicked = False
+
+        if event.key() == Qt.Key.Key_A or event.key() == Qt.Key.Key_Left:
+            self.l1 -= self.l1 / 1000
+            clicked = True
+        elif event.key() == Qt.Key.Key_S or event.key() == Qt.Key.Key_Down:
+            self.l2 += self.l1 / 1000
+            clicked = True
+        if clicked:
+            self.set_image(self.mash, self.l1, self.l2)
+            self.update_image()
 
 
 if __name__ == '__main__':
