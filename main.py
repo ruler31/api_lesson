877a3afb0bd8307640a63f87d00fa3928a974e2a
valievma <maksim.v1liev@gmail.com>
2024-02-02 17:36:51 +0300

    2

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ SCREEN_SIZE = [600, 450]
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        self.getImage()
-        self.initUI()
+        self.get_image()
+        self.init_ui()
 
-    def getImage(self):
+    def get_image(self):
         mash = 0.02
         m = f'{mash},{mash}'
         map_request = f"http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn={m}&l=map"
@@ -26,24 +26,21 @@ class Example(QWidget):
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
 
-        # Запишем полученное изображение в файл.
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
 
-    def initUI(self):
+    def init_ui(self):
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
 
-        ## Изображение
         self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
         self.image.move(0, 0)
         self.image.resize(600, 450)
         self.image.setPixmap(self.pixmap)
 
-    def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
+    def close_event(self, event):
         os.remove(self.map_file)
 
 
